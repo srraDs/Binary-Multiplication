6e8dd500aad36b37380dc08b97e37649b5a87aa1
Jvvvvv20 <84612741+Jvvvvv20@users.noreply.github.com>
2022-12-06 22:30:33 +0800

    Delete scripts directory

--- scripts/demo.js
+++
@@ -1,7 +0,0 @@
-/**
- * File containing the function call for starting the demonstration (simulation).
- */
-
-$(function () {
-	demo();
-});
--- scripts/export.js
+++
@@ -1,7 +0,0 @@
-/**
- * File containing the function call for starting the export of demonstration to a text (.txt) file.
- */
-
-$(function () {
-	exportText();
-});
