1b4cf077883049b373ca2155678dbdc5e826be29
Jvvvvv20 <84612741+Jvvvvv20@users.noreply.github.com>
2022-12-07 08:05:16 +0800

    optimized pencil in string.js

--- scripts/algo-strings.js
+++ scripts/algo-strings.js
@@ -23,10 +23,6 @@ const pencilStepB = ``;
 
 const pencilStepC = ``;
 
-const pencilAlgo = `
-    ${pencilStepA}
-    ${pencilStepB}
-    ${pencilStepC}`;
 
 /* --- BOOTH'S ALGORITHM --- */
 const boothsIntro = ``;
